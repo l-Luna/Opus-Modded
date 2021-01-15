013c9089aa9c391b1f3dec5a5d50ee4ef7d05d83
Luna <izzeldeen03@gmail.com>
2021-01-15 09:33:44 +0000

    No moe invalid names at all

--- RemappingVisitor.cs
+++ RemappingVisitor.cs
@@ -26,11 +26,11 @@ namespace Modded_Opus {
 				nonsense = IdentifierCollectingVisitor.intermediary[nonsense];
 			if(Program.mappings.ContainsKey(nonsense))
 				return Program.mappings[nonsense];
-			// if we've got something that starts with "#=", i.e. nonsense still, we'll remap that to "p" + the hash of the nonsense
+			// if we've got something invalid, i.e. nonsense still or "CS$<>___8" whatever, we'll remap that to "p" + the hash of the nonsense
 			// this should effectively only touch locals
 			// and is just for getting this to compile
 			// in the future a better solution can be made
-			if(nonsense.StartsWith("#="))
+			if(nonsense.StartsWith("#=") || nonsense.Contains("$") || nonsense.Contains("<"))
 				return ("p" + nonsense.GetHashCode()).Replace("-", "m");
 			return nonsense;
 		}
