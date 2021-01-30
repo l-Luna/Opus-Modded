f020c358a800a2ceb208e3c7eb47b79e2baca68d
Luna <izzeldeen03@gmail.com>
2021-01-30 14:06:58 +0000

    Fix Current references

--- CleanupVisitor.cs
+++ CleanupVisitor.cs
@@ -1,10 +1,8 @@
 ﻿using ICSharpCode.Decompiler.CSharp.Syntax;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace Modded_Opus{
+namespace Modded_Opus {
 	class CleanupVisitor : DepthFirstAstVisitor {
 
 		// steamworks got a refactor, so DispatchDelegate<T> is now Callback<T>.DispatchDelegate
@@ -13,21 +11,36 @@ namespace Modded_Opus{
 		// remove annotations at start of file
 
 		// Current has to be an accessor
-		// so Current(){ return ...; } has to be replaced with Current => ...;
+		public override void VisitMethodDeclaration(MethodDeclaration method) {
+			base.VisitMethodDeclaration(method);
+			// the method has to be mapped to Current or it won't compile anyways
+			// just... don't map anything to Current that looks like Enumerator's current
+			if(method.Name.Equals("Current") && method.Body.Count() == 1 && method.Body.First() is ReturnStatement){
+				var prop = new PropertyDeclaration();
+				prop.ReturnType = method.ReturnType.Clone();
+				prop.Name = (string)method.Name.Clone();
+				prop.PrivateImplementationType = method.PrivateImplementationType.Clone();
+				var acc = new Accessor();
+				acc.AddChild(new CSharpTokenNode(TextLocation.Empty, PropertyDeclaration.GetKeywordRole), PropertyDeclaration.GetKeywordRole);
+				acc.Body = (BlockStatement)method.Body.Clone();
+				prop.Getter = acc;
+				method.ReplaceWith(prop);
+			}
+		}
 
 		// "ref" should be replaced with "out"
 
 		// replace method_505 with the actual string
-		public override void VisitInvocationExpression(InvocationExpression invocationExpression) {
-			base.VisitInvocationExpression(invocationExpression);
+		public override void VisitInvocationExpression(InvocationExpression invocation) {
+			base.VisitInvocationExpression(invocation);
 			if(Program.Strings.Count() > 0) {
 				// maybe it'd be better to reference the original name, or ensure this gets a unique name... or just don't map it? assuming it never comes up
-				if(invocationExpression.Target.LastChild is Identifier id) {
+				if(invocation.Target.LastChild is Identifier id) {
 					// maybe it'd be better to reference the original name, or ensure this gets a unique name... or just don't map it? assuming it never comes up
 					if(RemappingVisitor.IntermediaryWhenMapped(id.Name).Equals("method_505")) {
-						if(invocationExpression.Arguments.Count == 1 && invocationExpression.Arguments.First() is PrimitiveExpression p && p.Value is int val) {
+						if(invocation.Arguments.Count == 1 && invocation.Arguments.First() is PrimitiveExpression p && p.Value is int val) {
 							if(Program.Strings.ContainsKey(val)) {
-								invocationExpression.ReplaceWith(new PrimitiveExpression(Program.Strings[val]));
+								invocation.ReplaceWith(new PrimitiveExpression(Program.Strings[val]));
 							} else {
 								Console.WriteLine("Missing string for " + val + "!");
 							}
